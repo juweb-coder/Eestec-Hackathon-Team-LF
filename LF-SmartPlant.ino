980d17e0ec78c86a95a160ec54eecf75e54cb6a8
juweb-coder <72350487+juweb-coder@users.noreply.github.com>
2020-10-13 19:24:04 +0200

    Add files via upload

--- LF-SmartPlant.ino
+++ LF-SmartPlant.ino
@@ -1,98 +1,177 @@
+//------------------------------//
 //TEAM LF - EESTEC HACKATHON//
-
- *   List of items used in this project and where to find them:
-■ Arduino Nano: https://geni.us/ArduinoNanoV3
-■ Mini submersible pump: https://geni.us/MiniPump
-■ 5mm tubing: https://geni.us/5mmTubing
-■ Transistor: https://geni.us/2npn2222
-■ Resistors (1k and 4.7k): https://geni.us/Ufa2s
-■ Wire: https://geni.us/22AWGWire
-■ 3mm LED: https://geni.us/LEDs
-■ Water level sensor: https://geni.us/WaterLevelSensor
-■ Nuts and Bolts: http://geni.us/NutsAndBolts
-■ Soil moisture sensor: https://geni.us/MoistureSensor
-■ Half Perma-proto board: https://geni.us/HalfPermaProto
-■ PLA Filament: https://geni.us/PLAFilament
-■ Battery powered glue gun: http://geni.us/BoschBatteryGlueGun
-■ Battery powered soldering iron: http://bit.ly/SealeySDL6
- */
-
-// These constants won't change. They're used to give names to the pins used:
-const int ledPin = 2;                                             // Digital output pin that the LED is attached to
-const int pumpPin = 12;                                           // Digital output pin that the water pump is attached to
-const int waterLevelPin = A3;                                      // Analoge pin water level sensor is connected to
-const int moistureSensorPin = 7;                                  // Digital input pin used to check the moisture level of the soil
+//------------------------------//
 
 
-// These are the values to edit - see the instructional video to find out what needs adjusting and why:
+//---------------------------------------------------------------------------------------------//
+//LIBRARIES//
 
-double checkInterval = 1800;                                      //time to wait before checking the soil moisture level - default it to an hour = 1800000
-int waterLevelThreshold = 380;                                    // threshold at which we flash the LED to warn you of a low water level in the pump tank - set this as per the video explains
-int emptyReservoirTimer = 90;                                     // how long the LED will flash to tell us the water tank needs topping up - default it to 900 = 30mins
-int amountToPump = 300;                                           // how long the pump should pump water for when the plant needs it
+#include <Arduino.h>
+#include <dht.h>
+dht DHT;
+//---------------------------------------------------------------------------------------------//
 
-// Global temp values
 
-int sensorWaterLevelValue = 0;                                      // somewhere to store the value read from the waterlevel sensor
-int moistureSensorValue = 0;                                       //somewhere to store the value read from the soil moisture sensor
+//---------------------------------------------------------------------------------------------//
+//CAPACITIVE MOISTURE SENSOR//
+
+const int moistureSensorPin = A0; // Analog pin sensor is connected to
+const int dry = 600; //calibration Values for the sensor min/max 
+const int wet = 250; //calibration Values for the sensor min/max 
+int moistureSensorValue = 0; //somewhere to store the value read from the soil moisture sensor
+int moisturePercentage = 0;
+
+//---------------------------------------------------------------------------------------------//
+
+
+//---------------------------------------------------------------------------------------------//
+//TEMPERATURE AND HUMIDITY SENSOR//
+
+const int tempHumiditySensorPin = 8; // digital pin sensor is connected to
+int readTempHumiditySensor = 0; //somewhere to store the value read from the temperature and humidity sensor
+float temperatureFloat = 0.0;
+float humidityFloat = 0.0;
+int temperatureInt = 0;
+int humidityInt = 0;
+
+//---------------------------------------------------------------------------------------------//
+
+
+//---------------------------------------------------------------------------------------------//
+//LIGHT SENSOR//
+
+const int lightSensorPin = A1; // analog pin sensor is connected to
+const int light = 250; //calibration Values for the sensor min/max 
+const int dark = 50; //calibration Values for the sensor min/max 
+int lightSensorValue = 0; //somewhere to store the value read from the soil moisture sensor
+int lightPercentage = 0;
+
+//---------------------------------------------------------------------------------------------//
+
+
+//---------------------------------------------------------------------------------------------//
+//NPN TRANSISTOR//
+
+const int npnTransistor = 6; // digital pin transistor is connected to
+
+//---------------------------------------------------------------------------------------------//
+
+
+//---------------------------------------------------------------------------------------------//
+//TIMER//
+
+long checkIntervall = 600000; //after 10mins
+long time;
+unsigned long previousTime = 0;
+
+//---------------------------------------------------------------------------------------------//                  
+
+
+//---------------------------------------------------------------------------------------------//
+//GLOBAL VARIABLES//
+
+bool startup = true;
+
+//---------------------------------------------------------------------------------------------//   
+
   
 void setup() {
-  // put your setup code here, to run once:
-    Serial.begin(9600);  
-    pinMode(ledPin, OUTPUT);
-    pinMode(pumpPin, OUTPUT);
-    pinMode(moistureSensorPin, INPUT);
+  Serial.begin(9600);
 
-                                                                  //flash the LED five times to confirm power on and operation of code:
-     for (int i=0; i <= 4; i++){
-        digitalWrite(ledPin, HIGH);
-        delay(300);
-        digitalWrite(ledPin, LOW);
-        delay(300);
-      }
-      delay(2000);
+} //end setup
 
-    digitalWrite(ledPin, HIGH);                                   // turn the LED on 
-}
+
+
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
+//---------------------------------------------------------------------------------------------//
+//TIMER//
+
+time = millis() - previousTime;
   
-  sensorWaterLevelValue = analogRead(waterLevelPin);              //read the value of the water level sensor
-  Serial.print("Water level sensor value: ");                     //print it to the serial monitor
-  Serial.println(sensorWaterLevelValue);
-
-  if (sensorWaterLevelValue < waterLevelThreshold){               //check if we need to alert you to a low water level in the tank
-      for (int i=0; i <= emptyReservoirTimer; i++){  
-        digitalWrite(ledPin, LOW);
-        delay(1000);
-        digitalWrite(ledPin, HIGH);
-        delay(1000);
-      }
-  }
-  else {
-    digitalWrite(ledPin, HIGH);
-    delay(checkInterval);                                         //wait before checking the soil moisture level
-  }
+if(time >= checkIntervall) { //after time which is set
+  previousTime = time; 
+  startMeasuring();
+} else if(checkIntervall < 0) { //after time overflow 
+  previousTime = 0;
+} else if(startup) { //if started up  measure sensor values one time directly
+  startMeasuring();
+}
+//---------------------------------------------------------------------------------------------//
 
 
-// check soil moisture level
+//---------------------------------------------------------------------------------------------//
+//WIFI MODULE//
+/*used Data to transfer:
+ * Moisture: moisturePercentage
+ * Temperature: temperatureInt
+ * Humidity: humidityInt
+ * Light: lightPercentage
+ */
 
-      moistureSensorValue = digitalRead(moistureSensorPin);       //read the moisture sensor and save the value
-      Serial.print("Soil moisture sensor is currently: ");
-      Serial.print(moistureSensorValue);
-      Serial.println(" ('1' means soil is too dry and '0' means the soil is moist enough.)");
-
-      if (moistureSensorValue == 1){
-                                                                 //pulse the pump 
-        digitalWrite(pumpPin, HIGH);
-          Serial.println("pump on");
-        delay(amountToPump);                                      //keep pumping water
-        digitalWrite(pumpPin, LOW);
-         Serial.println("pump off");
-        delay(800);                                              //delay to allow the moisture in the soil to spread through to the sensor
-      }
-
-
+//---------------------------------------------------------------------------------------------//
 
 }
+
+
+
+void startMeasuring() {
+  
+  digitalWrite(npnTransistor, HIGH); //turn on Transistor to power up sensors
+  delay(10000); //delay to start up sensors 
+  
+//---------------------------------------------------------------------------------------------//
+//CAPACITIVE MOISTURE SENSOR//
+
+  
+moistureSensorValue = analogRead(moistureSensorPin);
+
+moisturePercentage = map(moistureSensorValue, wet, dry, 100, 0);
+
+if (moisturePercentage >= 100)
+{
+    moisturePercentage = 100;
+} 
+else if (moisturePercentage <= 0)
+{
+  moisturePercentage = 0;
+} 
+//---------------------------------------------------------------------------------------------//
+
+
+//---------------------------------------------------------------------------------------------//
+//TEMPERATURE AND HUMIDITY SENSOR//
+
+  
+readTempHumiditySensor = DHT.read11(tempHumiditySensorPin); //read DHT11 Sensor Signal
+temperatureFloat = DHT.temperature;
+humidityFloat = DHT.humidity;
+temperatureInt = (int)temperatureFloat; //typecat float to int
+humidityInt = (int)humidityFloat; //typecat float to int
+
+//---------------------------------------------------------------------------------------------//
+
+
+//---------------------------------------------------------------------------------------------//
+//LIGHT SENSOR//
+
+  
+lightSensorValue = analogRead(lightSensorPin);
+
+lightPercentage = map(lightSensorValue, dark, light, 0, 100);
+
+if (lightPercentage >= 100)
+{
+    lightPercentage = 100;
+} 
+else if (lightPercentage <= 0)
+{
+  lightPercentage = 0;
+} 
+//---------------------------------------------------------------------------------------------//
+
+digitalWrite(npnTransistor, LOW); //turn off Transistor to save energy
+startup = false;
+                           
+} //end loop
